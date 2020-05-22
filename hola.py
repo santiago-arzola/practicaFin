2dd15f05cc1ea7126c162af1541313c8e3dc08c9
santiago-arzola <65579499+santiago-arzola@users.noreply.github.com>
2020-05-21 20:15:42 -0500

    uso
    primera

---
+++ hola.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def url_principal():
+	return render_template("template1.html", nombre="Santiago")
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
+	
